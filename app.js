fc0ef5264552a88e1cb775e34a6883a7f0bc8860
HAOXIANGW <haoxiangw@student.unimelb.edu.au>
2020-04-13 14:38:46 +0800

    testing

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
  res.send('<H1>Library System</H1>')
 });
-app.listen(3000, () => {
- console.log('The library app is listening on port 3000!')
+app.listen(PORT, () => {
+ console.log('The library app is listening on port ${PORT}!')
 });
