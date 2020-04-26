645fa7b9aa7c9af1a50adc77ffc990224eb627d2
HAOXIANGW <haoxiangw@student.unimelb.edu.au>
2020-04-27 01:22:29 +0800

    fix bug

--- routes/itemSearchRouter.js
+++ routes/itemSearchRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 // add our router
 const searchRouter = express.Router();
 // require the author controller
-const searchController = require('../controllers/itemSearchController');
+const searchController = require('../controllers/itemSearchController.js');
 
 // handle the GET request on root of author-management path,
 // i.e. get all authors
