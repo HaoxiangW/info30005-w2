dd0bf2c4d1ce0c0ae17df009a60d518a3c5ebd90
HAOXIANGW <haoxiangw@student.unimelb.edu.au>
2020-04-27 01:36:22 +0800

    Update itemSearchRouter.js

--- routes/itemSearchRouter.js
+++ routes/itemSearchRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 // add our router
 const searchRouter = express.Router();
 // require the author controller
-const searchController = require('../controllers/itemSearchController.js');
+
 
 // handle the GET request on root of author-management path,
 // i.e. get all authors
@@ -10,8 +10,7 @@ searchRouter.get('/', (req, res) => {
 	res.send('<H1>INDEX</H1><div class="searcher"><input type="text" name="q" placeholder="Search.."><button type="submit">Submit</button></div>')
 });
 
-//get author by id
-searchRouter.get('/:item', (req, res) => searchController.getAllItems(req, res));
+
 
 // export the router
 module.exports = itemSearchRouter;
