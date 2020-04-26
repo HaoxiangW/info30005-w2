297c2ecd95c3cd60a8d428acfe9388a599da801a
HAOXIANGW <haoxiangw@student.unimelb.edu.au>
2020-04-27 01:32:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // set up author routes
-const searchRouter = require('./routes/itemSearchRouter');
+const searchRouter = require('./routes/itemSearchRouter.js');
 
 app.get('/', (req, res) => {
  res.send('<H1>INDEX</H1><a href = "/search">search</a>')
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 
 // Handle search requests
 // the search routes are added onto the end of '/search'
-app.use('/search', searchRouter);
+
 
 app.listen(PORT, () => {
  console.log('This app is listening on port ${PORT}!')
