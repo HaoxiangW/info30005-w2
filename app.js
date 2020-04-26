186ee7eb33f49c8face4ed989ca1e6d5f0c9849b
HAOXIANGW <haoxiangw@student.unimelb.edu.au>
2020-04-26 14:53:37 +0800

    start search function

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // set up author routes
-const authorRouter = require('./routes/authorRouter');
+const searchRouter = require('./routes/searchRouter');
 
 app.get('/', (req, res) => {
- res.send('<H1>Library System</H1><a href = "/author-management">author</a>')
+ res.send('<H1>INDEX</H1><a href = "/search">search</a>')
 });
 
 // Handle author-management requests
 // the author routes are added onto the end of '/author-management'
-app.use('/author-management', authorRouter);
+app.use('/search', searchRouter);
 
 app.listen(PORT, () => {
- console.log('The library app is listening on port ${PORT}!')
+ console.log('This app is listening on port ${PORT}!')
 });
--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,14 +1,14 @@
 // provide the controller a link to the author model
-var authors = require('../models/author');
+var searchs = require('../models/author');
 // Function to handle a request to get all authors
 const getAllAuthors = (req, res) => {
-	res.send(authors); // return the list of authors
+	res.send(searchs); // return the list of authors
 };
 
 // Function to handle a request to a particular author
 const getAuthorByID = (req, res) => {
 // search for author in the database via ID
-const author = authors.find(author => author.id === req.params.id);
+const author = searchs.find(author => author.id === req.params.id);
 if (author){
 res.send(author); // send back the author details
 }
